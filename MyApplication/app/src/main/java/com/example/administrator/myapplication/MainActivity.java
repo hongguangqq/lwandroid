a87c5a6bee9d14705e703180031a40e7e115440a
hongguangqq <m13502562518@163.com>
2017-08-09 19:10:06 +0800

    第一次提交

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.administrator.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.github.chrisbanes.photoview.PhotoView;
+import com.jzxiang.pickerview.TimePickerDialog;
+import com.jzxiang.pickerview.data.Type;
+import com.jzxiang.pickerview.listener.OnDateSetListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        photoviewTest();
+    }
+
+    private void photoviewTest() {
+        PhotoView pv_test= (PhotoView) findViewById(R.id.pv_main_test);
+        TimePickerDialog pickerDialog=new TimePickerDialog.Builder()
+                .setCallBack(new OnDateSetListener() {
+                    @Override
+                    public void onDateSet(TimePickerDialog timePickerView, long millseconds) {
+                        Log.e("ZDLW","daat: "+millseconds+"  dataString:  "+DateUtils.timetByOrginStr(String.valueOf(millseconds)));
+                    }
+                })
+                .setTitleStringId("")
+                .setCyclic(false)
+                .setSelectorMillseconds(System.currentTimeMillis())
+                .setType(Type.YEAR_MONTH_DAY)
+                .setWheelItemTextSize(12)
+                .build();
+        pickerDialog.show(getSupportFragmentManager(),"");
+
     }
 }
